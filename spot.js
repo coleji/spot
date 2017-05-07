595fde55d2f496f72b48a49aa21a6223ced0c4af
Jonathan <coleji@bc.edu>
2017-05-07 19:38:19 -0400

    rewrote in a functional way in anticipation of ai simulator

--- spot.js
+++ spot.js
@@ -1,157 +1,152 @@
 var BOARD_SIZE = 7;
-var AI_MODE = true;
+var AI_MODE = false;
 
-var gameState = new function() {
-	var self = this;
-	this.isPlayersTurn = true;
-	this.selectedCell = null;
-	this.board = [];
+var Global = {
+	domNodes : [],
+	board : [],
+	highlightedCell : [],
+	isPlayersTurn : true
+};
 
-	this.occupy = function(target, is2Jump) {
-		var occupyingPlayer = self.selectedCell.state.owner;
-		var opponent = (occupyingPlayer == 'P') ? 'C' : 'P';
-		target.state.owner = occupyingPlayer;
-		for (var row=0; row<self.board.length; row++) {
-			for (var col=0; col<self.board[row].length; col++) {
-				if (self.board[row][col].isOtherCellXAway(target.row, target.col, 1) && self.board[row][col].state.owner == opponent) {
-					self.board[row][col].state.owner = occupyingPlayer;
-					self.board[row][col].draw();
+var uiProcs = {
+	paint: function() {
+		Global.board.forEach(function(row, i) {
+			row.forEach(function(owner, j) {
+				Global.domNodes[i][j].className = "cell " + (function() {
+					var color = (function() {
+						switch(owner) {
+						case 'P':
+							return "blue";
+						case 'C':
+							return "red";
+						default:
+							return "";
+						}
+					}());
+					if (Global.highlightedCell[0] == i && Global.highlightedCell[1] == j) return color + '-selected';
+					else return color;
+				}());
+			})
+		})
+	},
+	doClick : function(row, col, isHuman) {
+		if (AI_MODE && isHuman && !Global.isPlayersTurn) return;	// Prevent player from clicking for the AI while thinking
+		var makingAMove = false;
+		if (Global.highlightedCell.length > 0) {
+			// Something is highlighted
+			if (Global.highlightedCell[0] == row && Global.highlightedCell[1] == col) {
+				// Clicked the thing that is highlighted; unselect
+				Global.highlightedCell = [];
+			} else if (Global.board[row][col] == Global.board[Global.highlightedCell[0]][Global.highlightedCell[1]]) {
+				// Clicked another cell we own, highlight that instead
+				Global.highlightedCell = [row, col];
+			} else if (null == Global.board[row][col]) {
+				// Clicked an empty cell; can we move there?
+				makingAMove = true;
+				var newBoard = applyMove(Global.board, Global.highlightedCell[0], Global.highlightedCell[1], row, col);
+				if (null == newBoard) return;
+				else {
+					Global.board = newBoard;
+					Global.highlightedCell = [];
+					Global.isPlayersTurn = !Global.isPlayersTurn;
 				}
 			}
-		}
-		target.draw();
-		self.selectedCell.state.highlighted = false;
-		if (is2Jump) self.selectedCell.state.owner = null;
-		self.selectedCell.draw();
-		self.selectedCell = null;
-		this.isPlayersTurn = !this.isPlayersTurn;
-		if (AI_MODE && !this.isPlayersTurn) setTimeout(function() { doAITurn(this.board) }, 500);
-	}
-}
-
-function Cell(node, row, col) {
-	var self = this;
-	this.state = {};
-	this.row = row;
-	this.col = col;
-
-	this.isOtherCellXAway = function(otherRow, otherCol, x) {
-		var upRow = row-x;
-		var downRow = row+x;
-		var leftCol = col-x;
-		var rightCol = col+x;
-		return (
-			(otherRow == upRow && otherCol == rightCol) || // top right corner
-			(otherRow == upRow && otherCol == leftCol) || // top left corner
-			(otherRow == downRow && otherCol == rightCol) || // bottom right corner
-			(otherRow == downRow && otherCol == leftCol) || // bottom left corner
-			(otherRow == upRow && otherCol < rightCol && otherCol > leftCol) || // top row
-			(otherRow == downRow && otherCol < rightCol && otherCol > leftCol) || // bottom row
-			(otherCol == leftCol && otherRow > upRow && otherRow < downRow) || // left side
-			(otherCol == rightCol && otherRow > upRow && otherRow < downRow) // right side
-		);
-	}
-
-
-	this.draw = function() {
-		var colorClass = (function() {
-			if (self.state.owner == 'P') {
-				if (self.state.highlighted) return 'blue-selected'
-				else return 'blue';
-			} else if (self.state.owner == 'C') {
-				if (self.state.highlighted) return 'red-selected'
-				else return 'red';
-			} else return "";
-		}());
-		node.className = "cell " + colorClass
-	};
-
-	this.giveToPlayer = function() {
-		self.state.owner = 'P';
-		this.draw();
-	};
-	this.giveToComputer = function() {
-		self.state.owner = 'C';
-		this.draw();
-	};
-
-	this.doClick = function(isHuman) {
-		if (AI_MODE && !gameState.isPlayersTurn && isHuman) return;	// dont let the player be able to click red squares while computer is thinking
-		if (gameState.selectedCell) {		// Something is highlighted
-			if (self.state.highlighted) {				// Clicked the thing that is highlighted; unselect
-				self.state.highlighted = false;
-				gameState.selectedCell = null;
-				self.draw();
-			} else if (self.state.owner == gameState.selectedCell.state.owner) {	// Clicked another cell we own, highlight that instead
-				gameState.selectedCell.state.highlighted = false;
-				gameState.selectedCell.draw();
-				gameState.selectedCell = self;
-				self.state.highlighted = true;
-				self.draw();
-			} else if (self.state.owner == null) {	// Clicked an empty cell; can we move there?
-				if (self.isOtherCellXAway(gameState.selectedCell.row, gameState.selectedCell.col, 1)) {
-					gameState.occupy(self, false);
-				} else if (self.isOtherCellXAway(gameState.selectedCell.row, gameState.selectedCell.col, 2)) {
-					gameState.occupy(self, true);
-				}
-			} // else NOOP
 		} else {
-			var canClick = (function() {
-				if (self.state.owner == 'P') return gameState.isPlayersTurn
-				else if (self.state.owner == 'C') return !gameState.isPlayersTurn
-			  	else return false
-			}());
-			if (!canClick) return; //NOOP
-			self.state.highlighted = true;
-			gameState.selectedCell = self;
-			self.draw();
+			// just selecting a cell
+			var playerUp = (Global.isPlayersTurn ? 'P' : 'C')
+			if (Global.board[row][col] != playerUp) return;
+			Global.highlightedCell = [row, col];
 		}
-	};
-
-	// Construct
-	(function() {
-		self.state = {
-			owner : null,
-			highlighted : false
-		};
-		self.draw();
-		node.onclick = function() { self.doClick(true) };
-	}());
-}
-
-function getCell(row, col) {
-	return document.getElementById("cell_" + row + "_" + col);
-}
-
-function reset() {
-	var boardNode = document.getElementById("board");
-	var boardHTML = "<table><tbody>";
-	for (var row=0; row < BOARD_SIZE; row++) {
-		boardHTML += "<tr>";
-		for (var col=0; col < BOARD_SIZE; col++) {
-			boardHTML += "<td class='cell' id='cell_" + row + "_" + col + "'>&nbsp;</td>"
+		uiProcs.paint();
+		if (makingAMove && AI_MODE) {
+			setTimeout(function() {
+				var aiMoveObj = doAITurn();
+				Global.board = aiMoveObj.newBoard
+				Global.isPlayersTurn = true;
+			}, 500)
 		}
-		boardHTML += "</tr>";
+	},
+	reset : function() {
+		var boardNode = document.getElementById("board");
+		var boardHTML = "<table><tbody>";
+		for (var row=0; row < BOARD_SIZE; row++) {
+			boardHTML += "<tr>";
+			for (var col=0; col < BOARD_SIZE; col++) {
+				boardHTML += "<td class='cell' id='cell_" + row + "_" + col + "'>&nbsp;</td>"
+			}
+			boardHTML += "</tr>";
+		}
+		boardHTML += "</tbody></table>";
+		boardNode.innerHTML = boardHTML;
+
+		for (var row=0; row < BOARD_SIZE; row++) {
+			Global.domNodes[row] = [];
+			Global.board[row] = [];
+			for (var col=0; col < BOARD_SIZE; col++) {
+				Global.domNodes[row][col] = document.getElementById("cell_" + row + "_" + col);
+				Global.domNodes[row][col].onclick = (function(r, c) {
+					// forcing r and c to exist only in this scope and therefore retain their values
+					return function() { uiProcs.doClick(r, c, true); };
+				}(row, col));
+				Global.board[row][col] = null;
+			}
+		}
+
+		Global.board[0][0] = 'P';
+		Global.board[6][6] = 'P';
+		Global.board[0][6] = 'C';
+		Global.board[6][0] = 'C';
+		Global.isPlayersTurn = true;
+		uiProcs.paint();
 	}
-	boardHTML += "</tbody></table>";
-	boardNode.innerHTML = boardHTML;
+};
 
-	gameState.board = [];
-	for (var row=0; row < BOARD_SIZE; row++) {
-		gameState.board[row] = [];
-		for (var col=0; col < BOARD_SIZE; col++) {
-			gameState.board[row][col] = new Cell(document.getElementById("cell_" + row + "_" + col), row, col);
-		}
-	}
 
-	gameState.board[0][0].giveToPlayer();
-	gameState.board[6][6].giveToPlayer();
-	gameState.board[0][6].giveToComputer();
-	gameState.board[6][0].giveToComputer();
+// pure function
+// determine if two cells are x apart (i.e. is one cell on the edge of the square centered on the other cell, sides 2x+1 long)
+function areCellsXApart(row, col, otherRow, otherCol, x) {
+	var upRow = row-x;
+	var downRow = row+x;
+	var leftCol = col-x;
+	var rightCol = col+x;
+	return (
+		(otherRow == upRow && otherCol == rightCol) || // top right corner
+		(otherRow == upRow && otherCol == leftCol) || // top left corner
+		(otherRow == downRow && otherCol == rightCol) || // bottom right corner
+		(otherRow == downRow && otherCol == leftCol) || // bottom left corner
+		(otherRow == upRow && otherCol < rightCol && otherCol > leftCol) || // top row
+		(otherRow == downRow && otherCol < rightCol && otherCol > leftCol) || // bottom row
+		(otherCol == leftCol && otherRow > upRow && otherRow < downRow) || // left side
+		(otherCol == rightCol && otherRow > upRow && otherRow < downRow) // right side
+	);
 }
 
+// pure function
+// given an initial boardstate and a move, return the resulting boardstate
+function applyMove(initialBoard, fromRow, fromCol, toRow, toCol) {
+	if (initialBoard[toRow][toCol] != null) return null;  //cant move there; someone is already there
 
+	var is1Away = areCellsXApart(fromRow, fromCol, toRow, toCol, 1);
+	var is2Away = is1Away ? false : areCellsXApart(fromRow, fromCol, toRow, toCol, 2);
+	if (!is1Away && !is2Away) return null; // invalid move
 
-function doAITurn(board) {
+	var occupyingPlayer = initialBoard[fromRow][fromCol];
+	var opponent = (occupyingPlayer == 'P') ? 'C' : 'P';
+	return initialBoard.map(function(rowArr, row) {
+		return rowArr.map(function(owner, col) {
+			if (is2Away && row == fromRow && col == fromCol) return null;	// this is a jump, giving up my starting square
+			else if (row == toRow && col == toCol) return occupyingPlayer;	// square I'm moving into
+			else if (owner == opponent && areCellsXApart(toRow, toCol, row, col, 1)) return occupyingPlayer;	// surrounding opponent squares I'm taking
+			else return owner;
+		})
+	})
+}
+
+// return an obj of {fromRow, fromCol, toRow, toCol, newBoard}
+// so the calling function can slowly apply the move with step by step animations
+function doAITurn() {
 	console.log("AI Turn!")
+	return {
+		newBoard : Global.board
+	};
 }
